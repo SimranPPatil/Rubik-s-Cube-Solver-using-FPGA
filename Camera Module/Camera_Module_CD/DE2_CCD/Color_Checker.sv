eb51d5722a1d40584701a408e8c70a00d01d4212
Simran Pramod Patil <sppatil2@illinois.edu>
2017-04-29 16:20:26 -0500

    Added the signal from FSM for execution

--- Camera Module/Camera_Module_CD/DE2_CCD/Color_Checker.sv
+++ Camera Module/Camera_Module_CD/DE2_CCD/Color_Checker.sv
@@ -1,4 +1,4 @@
-module ColorChecker ( input logic Clk,
+module ColorChecker ( input logic Clk, cc,
 					  input logic [29:0] Color_in,
 					  output logic [29:0] Color_out);
 
@@ -12,20 +12,23 @@ Orange = 30'b111111111111101001010000000000;
 
 always_ff @(posedge Clk)
 begin
-	if( (Color_in - Red) / Red < 30'd0.05 || (Red - Color_in) / Red < 30'd0.05) 
-		Color_out <= Red;
-	else if ( (Color_in - Green) / Green < 30'd0.05 || (Green - Color_in) / Green < 30'd0.05) 
-		Color_out <= Green;
-	else if ( (Color_in - Blue) / Blue < 30'd0.05 || (Blue - Color_in) / Blue < 30'd0.05) 
-		Color_out <= Blue;
-	else if ( (Color_in - Yellow) / Yellow < 30'd0.05 || (Yellow - Color_in) / Yellow < 30'd0.05) 
-		Color_out <= Yellow;
-	else if ( (Color_in - White) / White < 30'd0.05 || (White - Color_in) / White < 30'd0.05) 
-		Color_out <= White;
-	else if ( (Color_in - Orange) / Orange < 30'd0.05 || (Orange - Color_in) / Orange < 30'd0.05) 
-		Color_out <= Orange;
-	else
-		Color_out <= 30'b000000000000000000000000000000;
+	if(cc)
+		begin
+			if( (Color_in - Red) / Red < 30'd0.05 || (Red - Color_in) / Red < 30'd0.05) 
+				Color_out <= Red;
+			else if ( (Color_in - Green) / Green < 30'd0.05 || (Green - Color_in) / Green < 30'd0.05) 
+				Color_out <= Green;
+			else if ( (Color_in - Blue) / Blue < 30'd0.05 || (Blue - Color_in) / Blue < 30'd0.05) 
+				Color_out <= Blue;
+			else if ( (Color_in - Yellow) / Yellow < 30'd0.05 || (Yellow - Color_in) / Yellow < 30'd0.05) 
+				Color_out <= Yellow;
+			else if ( (Color_in - White) / White < 30'd0.05 || (White - Color_in) / White < 30'd0.05) 
+				Color_out <= White;
+			else if ( (Color_in - Orange) / Orange < 30'd0.05 || (Orange - Color_in) / Orange < 30'd0.05) 
+				Color_out <= Orange;
+			else
+				Color_out <= 30'b000000000000000000000000000000;
+		end
 end
 
 endmodule
