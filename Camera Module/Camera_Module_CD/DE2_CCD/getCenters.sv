a0331671e2ed7bd712e74bfd1dfdb0095ec538b7
Simran Pramod Patil <sppatil2@illinois.edu>
2017-04-29 16:21:25 -0500

    Added the signal from FSM for execution

--- Camera Module/Camera_Module_CD/DE2_CCD/getCenters.sv
+++ Camera Module/Camera_Module_CD/DE2_CCD/getCenters.sv
@@ -1,4 +1,4 @@
-module getCenters ( input logic Clk,
+module getCenters ( input logic Clk,gc,
 					input logic [10:0] CubeX_Start, CubeY_Start,
 					input logic [9:0] N,
 					output logic [10:0] Block1X, Block1Y, 
@@ -15,34 +15,37 @@ module getCenters ( input logic Clk,
 
 always_ff @(posedge Clk)
 begin
-	Block1X <= CubeX_Start + N/6 ;
-	Block1Y <= CubeY_Start + N/6 ;
+	if(gc)
+		begin
+			Block1X <= CubeX_Start + N/6 ;
+			Block1Y <= CubeY_Start + N/6 ;
 
-	Block2X <= CubeX_Start + N/2 ;
-	Block2Y <= CubeY_Start + N/6 ;
+			Block2X <= CubeX_Start + N/2 ;
+			Block2Y <= CubeY_Start + N/6 ;
 
-	Block3X <= CubeX_Start + 5*N/6 ;
-	Block3Y <= CubeY_Start + N/6 ;
+			Block3X <= CubeX_Start + 5*N/6 ;
+			Block3Y <= CubeY_Start + N/6 ;
 
-	Block4X <= CubeX_Start + N/6 ;
-	Block4Y <= CubeY_Start + N/2 ;
+			Block4X <= CubeX_Start + N/6 ;
+			Block4Y <= CubeY_Start + N/2 ;
 
-	Block5X <= CubeX_Start + N/2 ;
-	Block5Y <= CubeY_Start + N/2 ;
+			Block5X <= CubeX_Start + N/2 ;
+			Block5Y <= CubeY_Start + N/2 ;
 
-	Block6X <= CubeX_Start + 5*N/6 ;
-	Block6Y <= CubeY_Start + N/2 ;
+			Block6X <= CubeX_Start + 5*N/6 ;
+			Block6Y <= CubeY_Start + N/2 ;
 
-	Block7X <= CubeX_Start + N/6 ;
-	Block7Y <= CubeY_Start + 5*N/6 ;
+			Block7X <= CubeX_Start + N/6 ;
+			Block7Y <= CubeY_Start + 5*N/6 ;
 
-	Block8X <= CubeX_Start + N/2 ;
-	Block8Y <= CubeY_Start + 5*N/6 ;
+			Block8X <= CubeX_Start + N/2 ;
+			Block8Y <= CubeY_Start + 5*N/6 ;
 
-	Block9X <= CubeX_Start + 5*N/6 ;
-	Block9Y <= CubeY_Start + 5*N/6 ;
-	
-	gotCenters <= 1'b1;
+			Block9X <= CubeX_Start + 5*N/6 ;
+			Block9Y <= CubeY_Start + 5*N/6 ;
+			
+			gotCenters <= 1'b1;
+		end
 end
 
 endmodule
