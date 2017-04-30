ffb32a3a2e9769d108bb404ae63dbc33f661e173
Simran Pramod Patil <sppatil2@illinois.edu>
2017-04-30 12:54:58 -0500

    Post debugging updated files

--- Camera Module/Camera_Module_CD/DE2_CCD/Color_Checker.sv
+++ Camera Module/Camera_Module_CD/DE2_CCD/Color_Checker.sv
@@ -3,28 +3,33 @@ module ColorChecker ( input logic Clk, cc,
 					  output logic [29:0] Color_out);
 
 logic [29:0] Red, Green, Blue, Yellow, White, Orange;
-Red = 30'b111111111100000000000000000000;
-Green = 30'b000000000011111111110000000000;
-Blue = 30'b000000000000000000001111111111;
-Yellow = 30'b111111111111111111110000000000;
-White = 30'b111111111111111111111111111111;
-Orange = 30'b111111111111101001010000000000;
+real a;
+initial
+begin
+	a = 0.05;
+end
+Register reg_red (.Clk(Clk), .init(init), .Load(1'b1), .Data_in(30'b111111111100000000000000000000), .Data_out(Red));
+Register reg_green (.Clk(Clk), .init(init), .Load(1'b1), .Data_in(30'b000000000011111111110000000000), .Data_out(Green));
+Register reg_blue (.Clk(Clk), .init(init), .Load(1'b1), .Data_in(30'b000000000000000000001111111111), .Data_out(Blue));
+Register reg_yellow (.Clk(Clk), .init(init), .Load(1'b1), .Data_in(30'b111111111111111111110000000000), .Data_out(Yellow));
+Register reg_white (.Clk(Clk), .init(init), .Load(1'b1), .Data_in(30'b111111111111111111111111111111), .Data_out(White));
+Register reg_orange (.Clk(Clk), .init(init), .Load(1'b1), .Data_in(30'b111111111111101001010000000000), .Data_out(Orange));
 
 always_ff @(posedge Clk)
 begin
 	if(cc)
 		begin
-			if( (Color_in - Red) / Red < 30'd0.05 || (Red - Color_in) / Red < 30'd0.05) 
+			if( (Color_in - Red) / Red < a || (Red - Color_in) / Red < a) 
 				Color_out <= Red;
-			else if ( (Color_in - Green) / Green < 30'd0.05 || (Green - Color_in) / Green < 30'd0.05) 
+			else if ( (Color_in - Green) / Green < a || (Green - Color_in) / Green < a) 
 				Color_out <= Green;
-			else if ( (Color_in - Blue) / Blue < 30'd0.05 || (Blue - Color_in) / Blue < 30'd0.05) 
+			else if ( (Color_in - Blue) / Blue < a || (Blue - Color_in) / Blue < a) 
 				Color_out <= Blue;
-			else if ( (Color_in - Yellow) / Yellow < 30'd0.05 || (Yellow - Color_in) / Yellow < 30'd0.05) 
+			else if ( (Color_in - Yellow) / Yellow < a || (Yellow - Color_in) / Yellow < a) 
 				Color_out <= Yellow;
-			else if ( (Color_in - White) / White < 30'd0.05 || (White - Color_in) / White < 30'd0.05) 
+			else if ( (Color_in - White) / White < a || (White - Color_in) / White < a) 
 				Color_out <= White;
-			else if ( (Color_in - Orange) / Orange < 30'd0.05 || (Orange - Color_in) / Orange < 30'd0.05) 
+			else if ( (Color_in - Orange) / Orange < a || (Orange - Color_in) / Orange < a) 
 				Color_out <= Orange;
 			else
 				Color_out <= 30'b000000000000000000000000000000;
