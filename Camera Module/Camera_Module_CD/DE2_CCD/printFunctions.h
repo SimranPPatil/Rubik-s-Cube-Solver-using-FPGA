646c47b56f158862cbdbfa46129854ac21a97b25
Simran Pramod Patil <sppatil2@illinois.edu>
2017-05-03 02:05:27 -0500

    Pio declarations

--- Camera Module/Camera_Module_CD/DE2_CCD/printFunctions.h
+++ Camera Module/Camera_Module_CD/DE2_CCD/printFunctions.h
@@ -1,6 +1,11 @@
 #ifndef PRINTFUNCTIONS_H
 #define PRINTFUNCTIONS_H
 
+#define to_hw_port_font (volatile char*) 0x00000080 // actual address here
+#define to_hw_sig_font (volatile char*) 0x00000060 // actual address here
+#define to_sw_port_font (char*) 0x00000070 // actual address here
+#define to_sw_sig_font (char*) 0x00000050 // actual address here
+
 extern FILE * fp;
 extern int * moves;
 extern int moveCounter;
